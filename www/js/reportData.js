2d115f1eb6c075f3e8de4d855de90a3b6943c1fc
Jaime Rosales <jaime.rosales.duque@autodesk.com>
2017-08-25 17:13:40 -0400

    adding licensing message to missing files

--- www/js/reportData.js
+++ www/js/reportData.js
@@ -1,5 +1,24 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
 //
-//  Getting relevant data for Report_barChart.js and Scheming.js
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+
+//
+//  Getting relevant data for Report_barChart.js
 //  The returned data format is only an object that uses label text(type/property value) as keys
 //  and the dbIds as the associated values. Some data wrapping might be necessary before use.
 //
