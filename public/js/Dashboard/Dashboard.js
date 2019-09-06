81f3b61229796b92c2fe74a659a2199287ed4e62
Augusto Goncalves <augusto.goncalves@autodesk.com>
2019-09-05 20:28:12 -0700

    copyright + fixes

--- public/js/Dashboard/Dashboard.js
+++ public/js/Dashboard/Dashboard.js
@@ -1,3 +1,21 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
 $(document).ready(function () {
     $(document).on('DOMNodeInserted', function (e) {
         if ($(e.target).hasClass('orbit-gizmo')) {
@@ -29,8 +47,8 @@ class Dashboard {
         // collumns so it can fit the new dashboard collumn
         var row = $(".row").children();
         $(row[0]).removeClass('col-sm-4').addClass('col-sm-2');
-        //$(row[1]).removeClass('col-sm-8').addClass('col-sm-5').after('<div class="col-sm-5" id="dashboard"></div>');
-        $(row[1]).after('<div class="col-sm-2" id="dashboard"></div>');
+        $(row[1]).removeClass('col-sm-8').addClass('col-sm-6').after('<div class="col-sm-4" id="dashboard"></div>');
+        //$(row[1]).after('<div class="col-sm-2" id="dashboard"></div>');
     }
 
     loadPanels = function () {
