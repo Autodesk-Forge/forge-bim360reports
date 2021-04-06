bc4c6a2406ce926428b7ac6b06ac28eef10087f2
jaimerosales <jaime.rosales.duque@autodesk.com>
2021-04-06 13:06:42 -0400

    fix of dashboard loading 2nd model reports and responsive for larger screens

--- public/js/Dashboard/Dashboard.js
+++ public/js/Dashboard/Dashboard.js
@@ -16,14 +16,12 @@
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////
 
-window.dashboard = null
-
 $(document).ready(function () {
     $(document).on('DOMNodeInserted', function (e) {
         if ($(e.target).hasClass('orbit-gizmo')) {
-            // to make sure we get the viewer, let's use the global var NOP_VIEWER
-            if (NOP_VIEWER === null || NOP_VIEWER === undefined) return;
-            dashboard = new Dashboard(NOP_VIEWER, [
+            // here, viewer represents the variable defined at viewer initialization
+            if (viewer === null || viewer === undefined) return;
+            new Dashboard(viewer, [
                 new BarChart('Type Name'),
                 new PieChart('Family Name')
             ])
@@ -37,10 +35,8 @@ class Dashboard {
         var _this = this;
         this._viewer = viewer;
         this._panels = panels;
-
+        this.adjustLayout();
         this._viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, (viewer) => {
-            this.adjustLayout();
-            setTimeout(function () { _this._viewer.resize() }, 500); _this._viewer.resize();
             _this.loadPanels();
         });
     }
@@ -49,15 +45,18 @@ class Dashboard {
         // this function may vary for layout to layout...
         // for learn forge tutorials, let's get the ROW and adjust the size of the 
         // columns so it can fit the new dashboard column, also we added a smooth transition css class for a better user experience
+        let dashdiv = document.getElementById('dashboard');
+        if(!!dashdiv){
+            dashdiv.parentElement.removeChild(dashdiv);
+        }
         var row = $(".row").children();
-        if (row.length === 3) return;
         $(row[0]).removeClass('col-sm-4').addClass('col-sm-2 transition-width');
         $(row[1]).removeClass('col-sm-8').addClass('col-sm-7 transition-width').after('<div class="col-sm-3 transition-width" id="dashboard"></div>');
     }
 
-    loadPanels() {
+    loadPanels () {
         var _this = this;
-        var data = new ModelData(this._viewer);
+        var data = new ModelData(this);
         data.init(function () {
             $('#dashboard').empty();
             _this._panels.forEach(function (panel) {
